c0d58a6739d44acb54da4e8c3cc9d37b8030201f
K Adam White <kadamwhite@users.noreply.github.com>
2022-02-01 11:53:31 -0500

    Implement note styling within editor.

--- src/blocks/editorial-comment/EditEditorialComment.js
+++ src/blocks/editorial-comment/EditEditorialComment.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { useBlockProps, RichText } from '@wordpress/block-editor';
 import { __ } from '@wordpress/i18n';
 
+import './style.scss';
+
 /**
  * Define the editor interface for an editorial comment.
  *
@@ -17,12 +19,12 @@ const EditEditorialComment = ( { attributes, setAttributes } ) => {
 	} );
 
 	return (
-		<div { ...blockProps }>
+		<div { ...blockProps } data-note-text={ __( 'Note', 'simple-editorial-comments' ) }>
 			<RichText
 				className="simple-editorial-comment__comment-text"
 				tagName="p"
 				value={ attributes.comment }
-				placeholder={ __( 'Leave an internal note about this post here...', 'simple-editorial-comments' ) }
+				placeholder={ __( 'Leave an internal note about this article here...', 'simple-editorial-comments' ) }
 				onChange={ ( comment ) => setAttributes( { comment } ) }
 			/>
 			<small>{ __( 'This comment will not render to users.', 'simple-editorial-comments' ) }</small>
